7d3622abcd1a4098bc4b838af414678d57b7ec3f
凯凯王 <9093108+kai_log@user.noreply.gitee.com>
2021-06-11 15:56:14 +0800

    完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+    <el-button type="info" @click='logout'>退出</el-button>
+</div>
+</template>
+<script>
+
+export default {
+
+props:{
+
+},
+data(){
+return {
+
+}
+},
+methods:{
+    logout() {
+        // 清空本地token
+        window.sessionStorage.clear();
+        // 跳转
+        this.$router.push('/login');
+    }
+},
+components:{
+
+},
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -3,9 +3,20 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// 引入全局样式
+import './assets/css/globle.css'
+
+import axios from 'axios'
+
+// 配置请求的根路径
+
+axios.defaults.baseURL = "http://timemeetyou.com:8889/api/private/v1/";
+
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
