3d812a647e3bef6da0043ec420baed0aa0802610
凯凯王 <9093108+kai_log@user.noreply.gitee.com>
2021-06-17 19:58:10 +0800

    '修改'

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 // 
-const prodplugins = []
+/* const prodplugins = []
 if (process.env.NODE_ENV === 'production') {
     prodplugins.push('transform-remove-console')
 }
@@ -13,4 +13,25 @@ module.exports = {
         ...prodplugins,
         '@babel/plugin-syntax-dynamic-import'
     ]
+} */
+
+//这是项目发布阶段用到的babel插件
+const prodplugins = [];
+if (process.env.NODE_ENV === "production") {
+    prodplugins.push("transform-remove-console");
 }
+
+module.exports = {
+    presets: ["@vue/cli-plugin-babel/preset"],
+    plugins: [
+        [
+            "component",
+            {
+                libraryName: "element-ui",
+                styleLibraryName: "theme-chalk",
+            },
+        ],
+        //发布产品时候的插件数组
+        ...prodplugins,
+    ],
+};
